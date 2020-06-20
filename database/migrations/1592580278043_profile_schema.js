a7a3d07b4f7ed1158a1088c815b31f2a1a56b7ea
desshub <korirdesmond@MacBook-Pro-3.local>
2020-06-20 14:47:01 +0300

    Create User and Profile models

---
+++ database/migrations/1592580278043_profile_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProfileSchema extends Schema {
+  up () {
+    this.create('profiles', (table) => {
+      table.increments()
+      table.string('name', 254).notNullable()
+      table.string('phone', 60).notNullable().unique()
+      table.string('gender', 60).nullable()
+      table.string('id_no', 60).nullable().unique()
+      table.integer('user_id', 60).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('profiles')
+  }
+}
+
+module.exports = ProfileSchema
